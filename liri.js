e237049eb554341c2e0f019346efa96f950c38a3
Tony Salvati <tsal2184@gmail.com>
2018-10-10 20:02:30 -0700

    liri-node-app

---
+++ liri.js
@@ -0,0 +1,50 @@
+var require = require("dotenv").config();
+var keys = require("./keys");
+var request = require("request");
+var movieName = process.argv[2];
+var spotify = require("spotify");
+var action = process.argv[2];
+var argument = "";
+
+doSomething(action, argument);
+
+switch (action) {
+    case "spotify-this-song":
+
+var songTitle = argument;
+if (songTitle === "") {
+    lookupSpecificSong();
+} else {
+    getSongInfo(songTitle);
+}
+break;
+    
+    case "movie-this":
+var movieTitle = argument;
+if (movieTitle === "") {
+    getMovieInfo("Forest Gump");
+} else {
+    getMovieInfo(movieTitle);
+}
+break;
+    case "do-what-it-says": 
+    doWhatItSays();
+break;
+}
+          
+   
+function movies(input) {
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "tt3896198&apikey=a11df5aa";
+            console.log(queryUrl);
+            request(queryUrl, function(error, response, body) {
+                if(!error && response.statusCode ===200) {
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                }
+            })
+        }   
